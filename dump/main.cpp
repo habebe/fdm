d961ae126288d6d335d01bfaad2924977ae09f06
habebe <hay.knock@gmail.com>
2017-12-28 12:11:43 -0600

    Added Qt-5 support.

--- dump/main.cpp
+++ dump/main.cpp
@@ -1,6 +1,7 @@
 #include "ModelGeometryLoader.hxx"
 #include "ModelGeometry.hxx"
 #include <iostream>
+#include <QCommandLineParser>
 
 void dump(const aiVector3D& v) {
 	std::cout << v.x << "," << v.y << "," << v.z << std::endl;
@@ -15,11 +16,28 @@ void dump(ModelGeometry* model) {
 	dump(model->max());
 }
 
+static void setupCommandLineParser(QCommandLineParser& parser) {
+	parser.setApplicationDescription("dumps geometry content to stdout.");
+	parser.addHelpOption();
+	parser.addVersionOption();
+	parser.addPositionalArgument("path", QCoreApplication::translate("path", "path of the file to import."));
+	
+	QCommandLineOption verboseOption("verbose", QCoreApplication::translate("main", "Verbose mode."));
+	parser.addOption(verboseOption);
+}
 
-int main(int argc,const char** argv) {
-	//std::cout << "ARGV:" << argv[1] << "\n";
+int main(int argc, char** argv) {
+	QCoreApplication app(argc, argv);
+	QCoreApplication::setApplicationName("dump");
+	QCoreApplication::setApplicationVersion("1.0");
+	QCommandLineParser parser;
+	setupCommandLineParser(parser);
+	parser.process(app);
+	const QStringList path = parser.positionalArguments();
 	ModelGeometryLoader* loader = ModelGeometryLoader::getInstance();
-	ModelGeometry* model = loader->load("d:\\dev\\build.test\\models\\OBJ\\box.obj");
+	//ModelGeometry* model = loader->load("d:\\dev\\build.test\\models\\OBJ\\box.obj");
+	ModelGeometry* model = loader->load(path.at(0).toStdString().c_str());
+
 	std::cout << "Model:" << model << "\n";
 	model->computeBoundingBox();
 	dump(model);
